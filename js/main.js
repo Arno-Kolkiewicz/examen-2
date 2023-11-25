f428da1743037ad61ec04ff11bc16d609b3cc6f4
Arno-Kolkiewicz <Arno.kolkiewicz@studentfr.ch>
2023-11-25 11:29:44 +0100

    html css tout fini (burger menu, plus que animation)

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@
 
 var burger = document.querySelector('.nav-burger');
 var menu = document.querySelector('.nav-line');
+var main = document.querySelector('.main');
+var footer = document.querySelector('.footer');
 
 var clickburger = function clickburger() {
     menu.classList.toggle('menu-active')
+    main.classList.toggle('main-close')
+    footer.classList.toggle('footer-close')
 };
 
 burger.addEventListener('click',clickburger);
@@ -15,7 +19,7 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
 	},
     navigation: {
-        nextEl: '.carousel-next',
-        prevEl: '.carousel-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 });
